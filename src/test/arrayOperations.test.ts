c978d99f9150719c66aeb4ffa009f7eaee2a47b0
patrifdz <fdzpatri@gmail.com>
2024-10-20 17:31:06 +0200

    test: add tests for arrayOperations file

--- src/test/arrayOperations.test.ts
+++ src/test/arrayOperations.test.ts
@@ -1,17 +1,61 @@
 import { expect, test, describe } from 'vitest';
+import { head,  tail, init, last } from '../arrayOperations';
+import { list1, list2 } from '../main';
 
-import { head } from '../arrayOperations';
- describe('Testing "language.ts"', ( ) => {
-test('Testing function "head"', () => {
-  // Arrange -> Definir los datos que usaremos para la prueba
-  const input = ["element1", "element2", "element3", "element4"];;
+describe('Testing "arrayOperations.ts"', ( ) => {
+            test('Testing function "head"', () => {
+              // Arrange -> Definir los datos que usaremos para la prueba
+              const input = list1;
 
-  // Act -> Ejecutas la acción que quieres probar, por ejemplo, ejecutas la función
-  const result = functionName(input);
+              // Act -> Ejecutas la acción que quieres probar, por ejemplo, ejecutas la función
+              const result = head(input);
 
-  // Assert -> Compruebas que el resultado es el esperado
-  expect(result).toBe(expectedValue);
-});
+              // Assert -> Compruebas que el resultado es el esperado
+              expect(result).toBe("element1");
+            });
+
+            test('Testing function "tail"', () => {
+              // Arrange -> Definir los datos que usaremos para la prueba
+              const input = list1;
+
+              // Act -> Ejecutas la acción que quieres probar, por ejemplo, ejecutas la función
+              const result = tail(input);
+
+              // Assert -> Compruebas que el resultado es el esperado
+              expect(result).toEqual(["element2", "element3", "element4"]);
+            });
+
+            test('Testing function "init"', () => {
+              // Arrange -> Definir los datos que usaremos para la prueba
+              const input = list1;
+              const input2 = list2;
+
+              // Act -> Ejecutas la acción que quieres probar, por ejemplo, ejecutas la función
+              const result = init(input);
+              const result2 = init(input2);
+
+              // Assert -> Compruebas que el resultado es el esperado
+              expect(result).toEqual(["element1", "element2", "element3"]);
+              expect(result2).toEqual([
+                "element1", 
+                "element2",
+                "element3",
+                "element4"]);
+            });
+
+            test('Testing function "last"', () => {
+              // Arrange -> Definir los datos que usaremos para la prueba
+              const input = list1;
+              const input2 = list2;
+
+              // Act -> Ejecutas la acción que quieres probar, por ejemplo, ejecutas la función
+              const result = last(input);
+              const result2 = last(input2);
+
+              // Assert -> Compruebas que el resultado es el esperado
+              expect(result).toEqual(["element4"])
+              expect(result2).toEqual(["element5"]);
+            });
 });
 
 
